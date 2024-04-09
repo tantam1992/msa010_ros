abc10674832f52d263cb7f0a08915a4fd540879c
chifongip <chifongip@github.com>
2024-04-09 12:59:43 +0800

    update cliff detector c++ version

--- src/cliff_detector.cpp
+++ src/cliff_detector.cpp
@@ -23,9 +23,35 @@ public:
   {
     nh_.param<std::string>("cliff_detector/img_topic", img_topic_, "depth/image_raw");
     nh_.param<std::string>("cliff_detector/cam_info_topic", cam_info_topic_, "depth/camera_info");
+    nh_.param<std::string>("cliff_detector/frame_id", frame_id_, "dep_cam_laser_link");
 
     nh_.param("cliff_detector/cam_height", cam_height_, 0.0);
     nh_.param("cliff_detector/cam_angle", cam_angle_, 0.0);
+    nh_.param("cliff_detector/cliff_threshold", cliff_threshold_, 0.0);
+    nh_.param("cliff_detector/img_freq", img_freq_, 0.0);
+    nh_.param("cliff_detector/range_min", range_min_, 0.0);
+    nh_.param("cliff_detector/range_max", range_max_, 0.0);
+    nh_.param("cliff_detector/cam_x", cam_x_, 0.0);
+    nh_.param("cliff_detector/cam_y", cam_y_, 0.0);
+    nh_.param("cliff_detector/cam_z", cam_z_, 0.0);
+
+    nh_.param("cliff_detector/row_upper", row_upper_, 0.0);
+    nh_.param("cliff_detector/col_left", col_left_, 0.0);
+    nh_.param("cliff_detector/col_right", col_right_, 0.0);
+
+    nh_.param("cliff_detector/skip_row_upper", skip_row_upper_, 0.0);
+    nh_.param("cliff_detector/skip_row_bottom", skip_row_bottom_, 0.0);
+
+    nh_.param("cliff_detector/tilt_compensation", tilt_compensation_, 0.0);
+    tilt_compensation_ = tilt_compensation_ * M_PI / 180.0;
+
+    kernel_ = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3));
+
+    trans_.setOrigin(tf::Vector3(cam_x_, cam_y_, cam_z_));          
+    q_.setRPY(0.0, (cam_angle_ * M_PI / 180.0), 0.0);
+    trans_.setRotation(q_);
+
+    camera_info_received_ = false;
 
     imu_sub_ = nh_.subscribe<sensor_msgs::Imu>("/imu/data", 1, &cliffDetector::imuCallback, this);
     cam_info_sub_ = nh_.subscribe<sensor_msgs::CameraInfo>(cam_info_topic_, 1, &cliffDetector::cameraInfoCallback, this);
@@ -70,38 +96,36 @@ public:
   }
 
 
-  // TODO: vertical_fov
   void calcDeltaAngleForImgRows(double vertical_fov) 
   {
-    const unsigned img_height = camera_model_.fullResolution().height;
-
-    delta_row_.resize(img_height);
-
     // Angle between ray and optical center
-    for(unsigned i = 0; i < img_height; i++) {
-      delta_row_[i] = vertical_fov * (i - camera_model_.cy() - 0.5) / (img_height - 1);
+    for(unsigned i = 0; i < img_height_; i++) 
+    {
+      delta_row_[i] = vertical_fov * (i - cy_ - 0.5) / (img_height_ - 1);
     }
   }
 
 
-  void calcGroundDistancesForImgRows() 
+  std::vector<double> calcGroundDistancesForImgRows(double cam_height, double cam_angle) 
   {
-    const unsigned img_height = camera_model_.fullResolution().height;
-    const double alpha = cam_angle_ * M_PI / 180.0; // Sensor tilt angle in radians
-
-    dist_to_ground_.resize(img_height);
+    std::vector<double> dist_to_ground(img_height_, 0.0);
+    double alpha = cam_angle * M_PI / 180.0; // Sensor tilt angle in radians
 
     // Calculations for each row of image
-    for (unsigned i = 0; i < img_height; i++) {
+    for (unsigned i = 0; i < img_height_; i++) 
+    {
       // Angle between ray and optical center
-      if ((delta_row_[i] + alpha) > 0) {
-        dist_to_ground_[i] = cam_height_ * sin(M_PI/2 - delta_row_[i])
-            / cos(M_PI/2 - delta_row_[i] - alpha);
+      if ((delta_row_[i] + alpha) > 0) 
+      {
+        dist_to_ground[i] = cam_height * sin(M_PI/2 - delta_row_[i]) / cos(M_PI/2 - delta_row_[i] - alpha);
       }
-      else {
-        dist_to_ground_[i] = 100.0;
+      else 
+      {
+        dist_to_ground[i] = 100.0;
       }
     }
+
+    return dist_to_ground;
   }
 
 
@@ -115,13 +139,160 @@ public:
   {
     camera_model_.fromCameraInfo(msg);
 
+    // For camera not rotate
+    /*
+    img_height_ = camera_model_.fullResolution().height;
+    img_width_ = camera_model_.fullResolution().width;
+    fx_ = camera_model_.fx();
+    fy_ = camera_model_.fy();
+    cx_ = camera_model_.cx();
+    cy_ = camera_model_.cy();
+    */
+
+    // For camera rotate 90deg (clockwise)
+    img_height_ = camera_model_.fullResolution().width;
+    img_width_ = camera_model_.fullResolution().height;
+    fx_ = camera_model_.fy();
+    fy_ = camera_model_.fx();
+    cx_ = camera_model_.fullResolution().height - camera_model_.cy();
+    cy_ = camera_model_.cx();
+
+    delta_row_.resize(img_height_);
+    dist_to_ground_.resize(img_height_);
+    dist_to_ground_init_.resize(img_height_);
+
+    fieldOfView(v_angle_min_, v_angle_max_, cx_, 0, cx_, cy_, cx_, img_height_ -1);
+    vertical_fov_ = v_angle_max_ - v_angle_min_;
+
+    calcDeltaAngleForImgRows(vertical_fov_);
+
+    dist_to_ground_init_ = calcGroundDistancesForImgRows(cam_height_, cam_angle_);
+
+    fieldOfView(h_angle_min_, h_angle_max_, 0, cy_, cx_, cy_, img_width_ - 1, cy_);
+    horizontal_fov_ = h_angle_max_ - h_angle_min_;
+
+    scan_msg_.header.frame_id = frame_id_;
+    scan_msg_.angle_min = h_angle_min_;
+    scan_msg_.angle_max = h_angle_max_;
+    scan_msg_.angle_increment = (scan_msg_.angle_max - scan_msg_.angle_min) / (img_width_ - 1);
+    scan_msg_.time_increment = 0.0;
+    scan_msg_.scan_time = 1 / img_freq_;
+    scan_msg_.range_min = range_min_;
+    scan_msg_.range_max = range_max_;
+
+    camera_info_received_ = true;
+
     cam_info_sub_.shutdown();
   }
 
 
   void depthImageCallback(const sensor_msgs::Image::ConstPtr& msg)
   {
+    if (!camera_info_received_)
+    {
+      ROS_WARN("Camera calibration parameters not received yet.");
+      return;
+    }
+
     tf::Matrix3x3(imu_quat_).getRPY(roll_, pitch_, yaw_);
+
+    if (abs(pitch_) > tilt_compensation_)
+    {
+      extra_height_ = cam_x_ * sin(pitch_);
+      dist_to_ground_ = calcGroundDistancesForImgRows(cam_height_ - extra_height_, cam_angle_ + (pitch_ * 180 / M_PI));
+    }
+    else
+    {
+      dist_to_ground_ = dist_to_ground_init_;
+    }
+
+    try
+    {
+      cv_ptr_ = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_8UC1);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+
+    cv::Mat img = cv_ptr_->image;
+    cv::rotate(img, img, cv::ROTATE_90_CLOCKWISE);
+
+    cv::Mat img_cliff = cv::Mat::zeros(100, 100, CV_8UC1);
+    cv::Mat img_edge = cv::Mat::zeros(100, 100, CV_8UC1);
+    
+    for (int col = col_left_; col < col_right_; col++)
+    {
+      for (int row = img_height_ - 1; row >= row_upper_; row--)
+      {
+        if (img.at<uchar>(row, col) == 255)
+        {
+          img_cliff.at<uchar>(row, col) = 127;
+        }
+        else
+        {
+          double dst = pow((img.at<uchar>(row, col) / 5.1), 2) / 1000;
+          if (dst < (dist_to_ground_[row] + cliff_threshold_))
+          {
+            img_cliff.at<uchar>(row, col) = 255;
+          }
+          else
+          {
+            img_cliff.at<uchar>(row, col) = 0;
+          }
+        }
+      }
+    }
+
+    cv::dilate(img_cliff, img_cliff, kernel_, cv::Point(-1, -1), 3);
+    cv::erode(img_cliff, img_cliff, kernel_, cv::Point(-1, -1), 3);
+
+    std::vector<double> p_dst(100, 0.0);
+
+    for (int col = col_left_; col < col_right_; col++)
+    {
+      for (int row = img_height_ - 1 - skip_row_bottom_; row >= row_upper_ + skip_row_upper_; row--)
+      {
+        if (img_cliff.at<uchar>(row, col) == 255 && (img_cliff.at<uchar>(row - 1, col) == 0 || img_cliff.at<uchar>(row - 1, col) == 127))
+        {
+          img_edge.at<uchar>(row, col) = 255;
+
+          double u = (col - cx_) / fx_;
+          double v = (row - cy_) / fy_;
+          double dst = pow((img.at<uchar>(row, col) / 5.1), 2) / 1000;
+
+          double x = dst;
+          double y = -dst * u;
+          double z = -dst * v;
+
+          p_.setOrigin(tf::Vector3(x, y, z));
+
+          p_trans_ = trans_ * p_;
+
+          p_dst[col] = sqrt(pow(p_trans_.getOrigin().x(), 2) + pow(p_trans_.getOrigin().y(), 2));
+
+          break;
+        }
+      }
+    }
+
+    scan_msg_.header.stamp = msg->header.stamp;
+
+    scan_msg_.ranges.resize(p_dst.size());
+    for (int i = 0; i < p_dst.size(); i++) {
+        scan_msg_.ranges[i] = p_dst[p_dst.size() - i - 1];
+    }
+
+    scan_pub_.publish(scan_msg_);
+
+    img_cliff_msg = cv_bridge::CvImage(std_msgs::Header(), "8UC1", img_cliff).toImageMsg();
+    img_cliff_msg->header = msg->header;
+    seg_pub_.publish(img_cliff_msg);
+
+    img_edge_msg = cv_bridge::CvImage(std_msgs::Header(), "8UC1", img_edge).toImageMsg();
+    img_edge_msg->header = msg->header;
+    cliff_pub_.publish(img_edge_msg);
   }
 
 
@@ -138,17 +309,42 @@ private:
 
   image_geometry::PinholeCameraModel camera_model_;
 
+  double img_height_, img_width_, fx_, fy_, cx_, cy_; 
+  bool camera_info_received_;
+
+  cv_bridge::CvImagePtr cv_ptr_;
+
   std::string img_topic_; 
   std::string cam_info_topic_;
+  std::string frame_id_;
+  double cam_height_, cam_angle_;
+  double cliff_threshold_;
+  double img_freq_, range_min_, range_max_;
+  double cam_x_, cam_y_, cam_z_;
+  double row_upper_, col_left_, col_right_;
+  double skip_row_upper_, skip_row_bottom_;
 
-  double cam_height_;
-  double cam_angle_;
+  double tilt_compensation_;
+  double extra_height_; 
+
+  double v_angle_min_, v_angle_max_, h_angle_min_, h_angle_max_;
+  double vertical_fov_, horizontal_fov_;
 
   std::vector<double> delta_row_;
-  std::vector<double> dist_to_ground_;
+  std::vector<double> dist_to_ground_, dist_to_ground_init_;
+
+  cv::Mat kernel_;
 
   tf::Quaternion imu_quat_; 
   double roll_, pitch_, yaw_; 
+
+  tf::Transform p_, trans_, p_trans_;
+  tf::Quaternion q_;
+
+  sensor_msgs::ImagePtr img_cliff_msg;
+  sensor_msgs::ImagePtr img_edge_msg;
+
+  sensor_msgs::LaserScan scan_msg_;
 };
 
 
